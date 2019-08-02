6dea1ad50b2438d0a3d0878d8b7a6173316a278d
DedeSnrwn13 <sunarwandede13@gmail.com>
2019-08-02 22:17:40 +0700

    scnd

--- ChartJS - DoughnutJS-1/DoughnutJS.js
+++ ChartJS - DoughnutJS-1/DoughnutJS.js
@@ -1,21 +1,58 @@
-$(document).ready(function(){
-	var ctx = $("#mycanvas").getContext("2d");
+// $(document).ready(function(){
+// 	var ctx = document.getElementById("mycanvas").getContext("2d");
 
-	var data = [
-		{
-			value: 160,
-			color: "cornfowerbue",
-			highlight: "lightskyblue",
-			label: "Saved"
-		},
-		{
+// 	var data = [
+// 		{
+// 			value: 160,
+// 			color: "cornfowerbue",
+// 			highlight: "lightskyblue",
+// 			label: "Saved"
+// 		},
+// 		{
 
-			value: 180,
-			color: "orange",
-			highlight: "darkorange",
-			label: "Total Spent"
-		}
-	]; 
+// 			value: 200,
+// 			color: "orange",
+// 			highlight: "darkorange",
+// 			label: "Total Spent"
+// 		}
+// 	]; 
 
-	var chart = new Chart(ctx).Doughnut(data);
-});
+// 	var chart = new Chart(ctx).Doughnut(data);
+// });
+
+
+
+
+const doughnutChart1 = function doughnutChart1(){
+    var doughnut = document.getElementById('mycanvas').getContext('2d');
+    var myChart = new Chart(doughnut, {
+        type: 'doughnut',
+        data:{
+            datasets: [{
+                data: [5,45,50],
+                backgroundColor:["#A3A1FB","#EDECFE","#3AE0F0"],
+                hoverBackgroundColor:["#A3A1FB","#EDECFE","#3AE0F0"],
+
+            }],
+
+            labels: [
+                'Sayur',
+                'Cuee',
+                'ayam',
+            ]
+        },
+        options:{   
+             rotation: (2*Math.PI) - (90 /180 * Math.PI),
+                cutoutPercentage: 75,
+
+
+             legend:{
+                display:false,
+             },
+        },
+    });
+
+}
+
+doughnut = doughnutChart1();
+
